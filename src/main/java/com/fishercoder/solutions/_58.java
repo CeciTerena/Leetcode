2b5eb376bae41c3a13ecca52ded87bd424bc4517
CeciTerena <ceciliaterena@gmail.com>
2024-06-14 12:00:25 +0200

    Added Flags to file 58 solution 1

--- src/main/java/com/fishercoder/solutions/_58.java
+++ src/main/java/com/fishercoder/solutions/_58.java
@@ -1,19 +1,34 @@
 package com.fishercoder.solutions;
+import java.util.HashMap;
+import java.util.Map;
 
 public class _58 {
-
     public static class Solution1 {
+        private static Map<String, Boolean> branchCoverage = new HashMap<>();
+
+        static {
+            branchCoverage.put("flag1", false);
+            branchCoverage.put("flag2", false);
+        }
+
         public int lengthOfLastWord(String s) {
             if (s == null || s.length() == 0) {
+                branchCoverage.put("flag1", true);
                 return 0;
             }
             s = s.trim();
             int n = s.length() - 1;
             while (n >= 0 && s.charAt(n) != ' ') {
+                branchCoverage.put("flag2", true);
                 n--;
             }
+            printCoverage();
             return s.length() - n - 1;
         }
+        public void printCoverage() {
+            for (Map.Entry<String, Boolean> entry : branchCoverage.entrySet()) {
+                System.out.println(entry.getKey() + " was " + (entry.getValue() ? "hit" : "not hit"));
+            }
+        }
     }
-
 }
