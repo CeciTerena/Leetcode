7f77e0011311b348e9d19371cd9715c34daf3f7b
Ava Frohna <122284870+avafrohna@users.noreply.github.com>
2024-06-14 11:17:31 +0200

    ava function 24

--- src/main/java/com/fishercoder/solutions/_24.java
+++ src/main/java/com/fishercoder/solutions/_24.java
@@ -25,16 +25,22 @@ public class _24 {
          * My completely original on 10/24/2021.
          */
         public ListNode swapPairs(ListNode head) {
+            boolean flag1 = false;
+            boolean flag2 = false;
+            boolean flag3 = false;
             ListNode pre = new ListNode(-1);
             pre.next = head;
             ListNode tmp = pre;
             while (head != null) {
+                flag1 = true;
                 ListNode third;
                 ListNode first = head;
                 ListNode second = head.next;
                 if (second == null) {
+                    flag2 = true;
                     break;
                 } else {
+                    flag3 = true;
                     third = head.next.next;
                     second.next = first;
                     first.next = third;
@@ -43,6 +49,9 @@ public class _24 {
                 }
                 head = third;
             }
+            System.out.println("Flag1: " + flag1 + "\n");
+            System.out.println("Flag2: " + flag2 + "\n");
+            System.out.println("Flag3: " + flag3 + "\n");
             return pre.next;
         }
     }
